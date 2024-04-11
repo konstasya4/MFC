ef05f4814c8a54097a21a0cd646847dfdea71f0b
Constanta <konstantinova-2002@mail.ru>
2024-04-11 15:32:54 +0300

    The authorization page has been made

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,43 +1,71 @@
-import React, { useContext } from "react";
-import { AuthContext, RoleContext } from "../context";
-import { Link } from "react-router-dom";
-const Login=()=>{
-    const {isAuth, setIsAuth} = useContext(AuthContext);
-    const{isRole, setIsRole}=useContext(RoleContext);
-    console.log(isAuth)
-    console.log(isRole)
+// import React, { useContext } from "react";
+// import { AuthContext, RoleContext } from "../context";
+// import { Link } from "react-router-dom";
+// const Login=()=>{
+//     const {isAuth, setIsAuth} = useContext(AuthContext);
+//     const{isRole, setIsRole}=useContext(RoleContext);
+//     console.log(isAuth)
+//     console.log(isRole)
 
-    // const login = event => {
-    //     event.preventDefault();
+//     // const login = event => {
+//     //     event.preventDefault();
         
-    // }
-    const admin = event =>{
-        // event.preventDefault();
-        setIsAuth(true);
-        localStorage.setItem('auth', 'true')
-        setIsRole('admin');
-        localStorage.setItem('role', 'admin')
-    }
-    const user = event =>{
-        // event.preventDefault();
-        setIsAuth(true);
-        localStorage.setItem('auth', 'true')
-        setIsRole('user');
-        localStorage.setItem('role', 'user')
-    }
-    return(
-        <div>
-            <form>
-                <input id="userName" type="text"/>
-                <input id="password" type="password"/>
-                <Link to="/admin"><button className="admin-button" onClick={admin}>
-                    Администратор
-                </button></Link>
-               <Link to="/mainUser"><button className="user-button"  onClick={user}>
-                Пользователь
-                </button></Link> 
-            </form>
+//     // }
+//     const admin = event =>{
+//         // event.preventDefault();
+//         setIsAuth(true);
+//         localStorage.setItem('auth', 'true')
+//         setIsRole('admin');
+//         localStorage.setItem('role', 'admin')
+//     }
+//     const user = event =>{
+//         // event.preventDefault();
+//         setIsAuth(true);
+//         localStorage.setItem('auth', 'true')
+//         setIsRole('user');
+//         localStorage.setItem('role', 'user')
+//     }
+//     return(
+//         <div>
+//             <form>
+//                 <input id="userName" type="text"/>
+//                 <input id="password" type="password"/>
+//                 <Link to="/admin"><button className="admin-button" onClick={admin}>
+//                     Администратор
+//                 </button></Link>
+//                <Link to="/mainUser"><button className="user-button"  onClick={user}>
+//                 Пользователь
+//                 </button></Link> 
+//             </form>
+//         </div>
+//     );
+// }
+// export default Login
+import React from 'react';
+import './LoginStyle.css';
+
+function Login ()  {
+    return (
+      <div className="login-container">
+        <div className="login-box">
+          <form>
+            <div className="textbox">
+            <label className='text-form' htmlFor="username">Логин</label>
+              <input className='input_form' type="text" placeholder="Введите ваш логин" name="username" required />
+            </div>
+            <div className="textbox">
+            <label  className='text-form' htmlFor="password">Пароль</label>
+              <input type="password" placeholder="Введите ваш пароль" name="password" required />
+            </div>
+          </form>
         </div>
+        <div className="external-buttons">
+            <button className="btn_login">Войти</button>
+          <a href="#" className='forgotPassword'>Забыли пароль?</a>
+        </div>
+      </div>
     );
 }
-export default Login
+
+export default Login;
+
