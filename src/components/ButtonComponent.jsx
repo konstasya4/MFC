abda5997a670605ca3bd7fb671fb78e99e8dcb1c
Constanta <konstantinova-2002@mail.ru>
2024-04-21 15:53:28 +0300

    Fixed the button component

--- src/components/ButtonComponent.jsx
+++ src/components/ButtonComponent.jsx
@@ -1,5 +1,5 @@
 import '../styles/componentsStyles/ButtonComponentStyle.css'
-const MainButton = ({className, name, onClick }) => {
+const ButtonComponent = ({className, name, onClick }) => {
   return <button className={`btn ${className}`} onClick={onClick}>{name}</button>;
 };
-export default MainButton;
+export default ButtonComponent;
