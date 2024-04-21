8314f5f0884f7545a5c9ffeabe664e4e4a145419
Constanta <konstantinova-2002@mail.ru>
2024-04-21 15:54:16 +0300

    Fixed the input component

--- src/components/InputComponent.jsx
+++ src/components/InputComponent.jsx
@@ -1,15 +1,48 @@
-import React from "react";
-// import './InputCompanentStyle.css'
+import React, { useState } from "react";
 import '../styles/componentsStyles/InputComponentStyle.css'
-const InputComponent =({ placeholder, ...restProps })=>{
-return(
-    <div>
-        <input className="input"
-        type="text"
+import Show from "../images/Show.png";
+import Hide from "../images/Hide.png";
+const InputComponent =({ className, placeholder, type, onChange, required, ...restProps  })=>{
+    const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+  return (type === "password" ?
+    <div className="input-password">
+      <input
+        className={className}
+        type={showPassword ? "text" : type}
         placeholder={placeholder}
+        onChange={onChange}
+        required={required}
         {...restProps}
-        />
-    </div>
-)
-}
+      />
+      {type === "password" ? (
+        showPassword?
+        <button className="show-btn" onClick={togglePasswordVisibility}>
+          <img src={Hide} />
+        </button>:
+        <button className="show-btn" onClick={togglePasswordVisibility}>
+          <img src={Show} />
+        </button>)
+        : null
+      }
+    </div>:
+    <div className={className}>
+    <input
+      type={showPassword ? "text" : type}
+      placeholder={placeholder}
+      onChange={onChange}
+      required={required}
+      {...restProps}
+    /></div>
+  );
+};
+
 export default InputComponent;
+
+
+
+
+
